77781593c175fa108aee142bf487d672f4611bc9
David Wood <git@david.davidtw.co>
2019-07-19 21:01:46 +0100

    Remove dependencies from derive crate.
    Co-authored-by: Rémy Rakić <remy.rakic@gmail.com>

--- derive-crate/src/lib.rs
+++ derive-crate/src/lib.rs
@@ -1,30 +1,36 @@
 extern crate proc_macro;
-#[macro_use]
-extern crate quote;
 
-use quote::TokenStreamExt;
+use proc_macro::{Group, TokenStream, TokenTree};
+
+// This macro exists as part of a reproduction of #61963 but without using quote/syn/proc_macro2.
 
 #[proc_macro_derive(DomObject)]
-pub fn expand_token_stream(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
-    let input: syn::DeriveInput = syn::parse(input).unwrap();
-    let fields = if let syn::Data::Struct(syn::DataStruct { ref fields, .. }) = input.data {
-        fields.iter().collect::<Vec<&syn::Field>>()
-    } else {
-        panic!("#[derive(DomObject)] should only be applied on proper structs")
-    };
+pub fn expand_token_stream(input: TokenStream) -> TokenStream {
+    // Construct a dummy span - `#0 bytes(0..0)` - which is present in the input because
+    // of the specially crafted generated tokens in the `attribute-crate` proc-macro.
+    let dummy_span = input.clone().into_iter().nth(0).unwrap().span();
 
-    let mut field_types = vec![];
-    for field in fields {
-        field_types.push(&field.ty);
+    // Define what the macro would output if constructed properly from the source using syn/quote.
+    let output: TokenStream = "impl Bar for ((), Qux<Qux<Baz> >) { }
+    impl Bar for ((), Box<Bar>) { }".parse().unwrap();
+
+    let mut tokens: Vec<_> = output.into_iter().collect();
+    // Adjust token spans to match the original crate (which would use `quote`). Some of the
+    // generated tokens point to the dummy span.
+    for token in tokens.iter_mut() {
+        if let TokenTree::Group(group) = token {
+            let mut tokens: Vec<_> = group.stream().into_iter().collect();
+            for token in tokens.iter_mut().skip(2) {
+                token.set_span(dummy_span);
+            }
+
+            let mut stream = TokenStream::new();
+            stream.extend(tokens);
+            *group = Group::new(group.delimiter(), stream);
+        }
     }
 
-    let mut items = quote! {};
-    items.append_all(field_types.iter().map(|ty| {
-        quote! {
-            impl Bar for ((), #ty) {}
-        }
-    }));
-
-    let x = quote! { #items };
-    x.into()
+    let mut output = TokenStream::new();
+    output.extend(tokens);
+    output
 }
