5fd9999dae70ae2e49239a3fe0974ba456dfc4f2
David Wood <git@david.davidtw.co>
2019-07-19 19:58:10 +0100

    Remove dependencies from attribute crate.

--- attribute-crate/src/lib.rs
+++ attribute-crate/src/lib.rs
@@ -1,18 +1,12 @@
 extern crate proc_macro;
-#[macro_use]
-extern crate quote;
-extern crate syn;
 
 use proc_macro::TokenStream;
-use syn::*;
 
 #[proc_macro_attribute]
-pub fn dom_struct(_: TokenStream, input: TokenStream) -> TokenStream {
-    let attributes: TokenStream =
-        "#[derive(DomObject)]".to_string().parse().unwrap();
-    let output: TokenStream = attributes.into_iter()
-        .chain(input.into_iter()).collect();
-
-    let item: Item = syn::parse(output).unwrap();
-    quote!(#item).into()
+pub fn dom_struct(_: TokenStream, _: TokenStream) -> TokenStream {
+    // This is hardcoded for the test case.
+    "#[derive(DomObject)] struct Foo {
+        qux: Qux<Qux<Baz> >,
+        bar: Box<Bar>,
+    }".parse().unwrap()
 }
