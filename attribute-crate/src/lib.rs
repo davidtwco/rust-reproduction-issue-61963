2baf09d6fc09c6becd820ee3e45d6d9bd242ee66
Rémy Rakic <remy.rakic@gmail.com>
2019-07-20 02:44:41 +0200

    note the space is important just in case

--- attribute-crate/src/lib.rs
+++ attribute-crate/src/lib.rs
@@ -4,7 +4,7 @@ use proc_macro::TokenStream;
 
 #[proc_macro_attribute]
 pub fn dom_struct(_: TokenStream, _: TokenStream) -> TokenStream {
-    // This is hardcoded for the test case.
+    // This is hardcoded for the test case, the space before the '>' is significant.
     "#[derive(DomObject)] struct Foo {
         qux: Qux<Qux<Baz> >,
         bar: Box<Bar>,
